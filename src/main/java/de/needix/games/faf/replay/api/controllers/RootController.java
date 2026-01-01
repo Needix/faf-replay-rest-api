9657b9659865350493dd2f6877b38a5d9e56d0d1
Need <need@needix.de>
2026-01-01 19:37:36 +0100

    Add utility to save entities with logging and exception handling
    Introduced a generic method `saveEntityInDatabase` to handle entity persistence with detailed logging, including execution time. Added error handling to log failures and rethrow exceptions, ensuring traceability and debugging support.

--- src/main/java/de/needix/games/faf/replay/api/controllers/RootController.java
+++ src/main/java/de/needix/games/faf/replay/api/controllers/RootController.java
@@ -1,5 +1,8 @@
 package de.needix.games.faf.replay.api.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +10,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 @CrossOrigin
 @Controller
 public class RootController {
+    public static final Logger LOGGER = LoggerFactory.getLogger(RootController.class);
 
     @GetMapping("/")
     public String redirectToSwagger() {
         return "redirect:/index.html";
+    }
+
+    public static <Repo extends CrudRepository<E, Long>, E> void saveEntityInDatabase(Repo repo, E entity) {
+        long startTime = System.currentTimeMillis();
+
+        try {
+            repo.save(entity);
+        } catch (Exception e) {
+            LOGGER.error("Failed to save entity to database: {}", e.getMessage(), e);
+            throw e;
+        }
+
+        if (LOGGER.isInfoEnabled()) {
+            long endTime = System.currentTimeMillis();
+            LOGGER.info("Entity {} saved in {} ms", entity, endTime - startTime);
+        }
     }
 }
